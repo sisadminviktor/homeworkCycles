366b6759a8413fdea005fd0be3ac8f1f6a6e4bf9
victor <victor@MacBook-Pro-victor.local>
2023-05-25 15:14:11 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,69 @@
+# #6.22(г,д)
+# n = int(input("enter your total\n"))
+# sum = 0
+# while n > 0:
+#     if n % 10 > 5:
+#         sum = sum + n%10
+#     n = n // 10
+# print(sum)
+
+# n = int(input("enter your total\n"))
+#
+# pr = 1
+# while n > 0:
+#     if n % 10 > 7:
+#         pr = pr * (n%10)
+#     n = n // 10
+# else:
+#     print("error")
+# print(pr)
+
+#6.23(a,в)
+
+# n = int(input("введите ваше число\n"))
+# a = int(input("введите вашу цифру\n"))
+# kol = 0
+# while n > 0:
+#     num = n % 10
+#     if a == num:
+#         kol = kol + 1
+#     n = n //10
+# print(kol)
+
+# n = int(input("вводим число\n"))
+# a = int(input("вводим число a\n"))
+# sum = 0
+# while n > 0 and a>=0 and a<=8:
+#     num = n % 10
+#     if num >a:
+#         sum = sum + num
+#     n = n // 10
+# print(sum)
+
+#6.27#
+# n = int(input("введите число\n"))
+# sum = 0
+# max = 0
+# min = 9
+# while n >0:
+#     num = n % 10
+#     if num >max:
+#         max = num
+#     if num < min:
+#         min = num
+#     sum = min + max
+#     n = n//10
+# print(sum)
+
+
+
+
+
+
+
+
+
+
+
+
+
